f4d80a3ed0d794ccaa128272a1bd0b48a9c68ea1
richardlee86 <richard.t.lee86@gmail.com>
2021-05-05 14:19:13 +0200

    Finished project to specifications.

--- responsive-react-webpage/src/App.js
+++ responsive-react-webpage/src/App.js
@@ -3,9 +3,67 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <div className="container">
+      <article class="content">
+        <h1>Learn to code by <br />watching others</h1>
+        <p class="content-paragraph">
+          See how experienced developers solve problems in real-time. Watching
+          scripted tutorials is great, but understanding how developers think is
+          invaluable.
+        </p>
+      </article>
+      <div class="form-container">
+        <div class="form-heading">
+          <h3>Try it free 7 days <span>then $20/mo thereafter</span></h3>
+        </div>
+        <form action="#">
+          <input
+            type="text"
+            name="first-name"
+            id="first-name"
+            placeholder="First Name"
+            required
+          />
+          <input
+            type="text"
+            name="last-name"
+            id="last-name"
+            placeholder="Last Name"
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+            required
+          />
+          <div class="check">
+            <input
+              class="password"
+              type="checkbox"
+              name="showPassword"
+              id="showPassword"              
+            /><label for="showPassword" class="password-label"
+              >Show Password</label
+            >
+          </div>
+
+          <button type="submit">CLAIM YOUR FREE TRIAL</button>
+          <label for=""
+            >By clicking the button you are agreeing to our
+            <span class="terms-span">Terms and Services</span></label
+          >
+        </form>
+      </div>
+      </div>    
   );
 }
 
